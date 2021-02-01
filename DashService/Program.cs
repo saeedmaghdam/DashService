95b27dbd34f58b1f674c9bc15b2eb3157f6d67fb
saeedmaghdam <me.register@outlook.com>
2021-02-01 22:02:25 +0330

    Added a console application

---
+++ DashService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DashService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Application");
+        }
+    }
+}

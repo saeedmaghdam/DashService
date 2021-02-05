df04448601182e474c1eb11d69a5ec9a7ed2fc8e
saeedmaghdam <me.register@outlook.com>
2021-02-06 02:28:05 +0330

    Send job status to client using socket

--- Projects/Application/Sources/DashService.WebApi/WebSocket/Job.cs
+++ Projects/Application/Sources/DashService.WebApi/WebSocket/Job.cs
@@ -16,25 +16,43 @@ namespace DashService.WebApi.WebSocket
 
             if (job.JobStartingTask.Status != TaskStatus.Running)
             {
-                job.StartCancellationTokenSource = new System.Threading.CancellationTokenSource();
-                job.StopCancellationTokenSource = new System.Threading.CancellationTokenSource();
+                job.StartCancellationTokenSource = new CancellationTokenSource();
+                job.StopCancellationTokenSource = new CancellationTokenSource();
                 job.JobStartingTask = Task.Run(() => { job.JobInstance.StartAsync(job.StartCancellationTokenSource.Token); }, CancellationToken.None);
+                Socket.CallClientMethod(@"
+{
+    ""command"": ""change_status"",
+    ""data"": {
+        ""view_id"": """ + job.JobInstance.GetType().GUID + @""",
+        ""status"": """ + TaskStatus.Running.ToString() + @"""
+    }
+}
+");
             }
 
             return Task.CompletedTask;
         }
 
         public static Task Stop(Guid jobViewId)
-        {   
+        {
             var job = Context.JobContainer.Jobs.Where(x => x.JobInstance.GetType().GUID == jobViewId).SingleOrDefault();
 
             if (job == null)
                 return Task.CompletedTask;
 
-            if (job.JobStartingTask.Status == TaskStatus.Running)
+            if (job.JobStartingTask.Status == TaskStatus.Running || job.JobStartingTask.Status == TaskStatus.WaitingForActivation || job.JobStartingTask.Status == TaskStatus.WaitingForChildrenToComplete || job.JobStartingTask.Status == TaskStatus.WaitingToRun)
             {
                 job.StartCancellationTokenSource.Cancel();
-                job.JobStoppingTask = Task.Run(() => { job.JobInstance.StopAsync(job.StopCancellationTokenSource.Token); }, CancellationToken.None); 
+                job.JobStoppingTask = Task.Run(() => { job.JobInstance.StopAsync(job.StopCancellationTokenSource.Token); }, CancellationToken.None);
+                Socket.CallClientMethod(@"
+{
+    ""command"": ""change_status"",
+    ""data"": {
+        ""view_id"": """ + job.JobInstance.GetType().GUID + @""",
+        ""status"": """ + TaskStatus.Canceled.ToString() + @"""
+    }
+}
+");
             }
 
             return Task.CompletedTask;

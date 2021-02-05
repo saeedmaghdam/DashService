1e9a25c8dc2e950096332dbeafdd4f2ca77df488
saeedmaghdam <me.register@outlook.com>
2021-02-05 17:34:24 +0330

    Added jobs list api

---
+++ Projects/Application/Sources/DashService.WebApi/ApiResult.cs
@@ -0,0 +1,14 @@
+﻿namespace DashService.WebApi
+{
+    public class ApiResult<T>
+    {
+        private readonly T _data;
+
+        public ApiResult(T data)
+        {
+            _data = data;
+        }
+
+        public T Data => _data;
+    }
+}
---
+++ Projects/Application/Sources/DashService.WebApi/Controllers/JobController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashService.WebApi.Controllers
+{
+    public class JobController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var jobs = Context.JobContainer.Jobs;
+
+            return await Task.FromResult(
+                Ok(new ApiResult<IEnumerable<dynamic>>(jobs.Select(job => new {
+                    FullName = job.JobInstance.GetType().FullName,
+                    Namespace = job.JobInstance.GetType().Namespace,
+                    Name = job.JobInstance.Name,
+                    Description = job.JobInstance.Description,
+                    JobStatus = job.JobStatus,
+                    Version = job.JobInstance.Version,
+                    ViewId = job.JobInstance.GetType().GUID
+                })))
+            );
+        }
+    }
+}
